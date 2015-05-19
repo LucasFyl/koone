be7e0ede263afa1a0c69f3404cfef1e526e367e9
Lucas F <lfayolle1@gmail.com>
2015-05-19 20:35:17 +0200

    setup lessjs

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -9,7 +9,7 @@
   <meta name="description" content="<?php echo $site->description()->html() ?>">
   <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
   <link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic|Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-  <?php echo css('assets/css/main.css') ?>
+  <?php echo css('assets/css/style.css') ?>
 
 </head>
 <body>
