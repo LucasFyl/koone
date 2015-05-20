81224141450a199ffad2b95faf25f3912f3e03ae
Lucas F <lfayolle1@gmail.com>
2015-05-20 12:47:18 +0200

    cleaning"

--- Gruntfile.js
+++ Gruntfile.js
@@ -132,15 +132,14 @@ module.exports = function(grunt) {
   });
 
   // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-less');
 
   // Default task.
   grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
   grunt.registerTask('test', ['jshint', 'qunit']);
-
 };
