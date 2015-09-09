d0ae91b03ef9fb1bb3c3fd1e4979fdad5f4b3cfd
Lucas Fayolle <lfayolle1@gmail.com>
2015-09-09 18:34:21 +0200

    after deployment content update

--- www/site/accounts/koone.php
+++ www/site/accounts/koone.php
@@ -8,3 +8,10 @@ password: >
   $2a$10$CN4NjkQOkBCilAV7O6Q.P.ul1xabt.txfBwqnCoTzfQlAVTQ40alm
 language: de
 role: admin
+history:
+  - null
+  - albums/Wang-Ramirez
+  - home
+  - wallpapers
+  - about
+token: 9e7f25934f89115bcf2090bd8e9b948ef8c9308d
